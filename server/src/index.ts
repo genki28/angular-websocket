b8e0af6e9a0adae09793d4cc40287fc693d8242e
GenkiOsawa <taiiku39@gmail.com>
2021-07-04 23:59:10 +0900

    wsテストしてみた

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,28 @@
 import express from 'express'
+import ws from 'ws'
 
 const app = express()
 const PORT = 8080
 
 app.get('/', (req, res) => {
   res.send({message: 'message'})
+})
+
+const server = new ws.Server({port: 5001})
+server.on('connection', ws => {
+  // クライアントからのデータ受信時に呼ばれるとのこと
+  ws.on('message', message => {
+    console.log(message)
+
+    server.clients.forEach(client => {
+      client.send(message)
+    })
+  })
+
+  // 接続時に呼ばれるとのこと
+  ws.on('close', () => {
+    console.log('close')
+  })
 })
 
 app.listen(PORT, () => {
